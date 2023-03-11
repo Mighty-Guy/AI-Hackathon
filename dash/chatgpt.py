10f703aaedb18bd8586944c4293ccd0e4828f24c
Richard Schlager <fhs48276@fh-salzburg.ac.at>
2023-03-11 14:45:12 +0100

    chatgpt added music recommendation

--- dash/chatgpt.py
+++ dash/chatgpt.py
@@ -3,6 +3,11 @@ import openai
 import yaml
 from yaml.loader import SafeLoader
 
+initPrompt = [
+    {"role": "system", "content": "You are a storyteller. Generate a text-based game adventure with 10 stages and 4 options per stage. Wait for my response at every stage."}
+]
+
+
 class ChatGPT():
     def __init__(self):
         # Reading YAML data
@@ -10,15 +15,13 @@ class ChatGPT():
         with open(file_name, 'r') as f:
             secrets = yaml.load(f, Loader=SafeLoader)
         openai.api_key = secrets['chatgpt']
-        self.conversation = [
-            {"role": "system", "content": "You are a storyteller. Generate a text-based game adventure with 10 stages and 4 options per stage. Wait for my response at every stage."}
-        ]
+        self.conversation = initPrompt
 
-    def get_response(self, prompt):
+    def get_story(self, prompt):
         self.conversation.append({"role": "user", "content": prompt})
         response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=self.conversation,    
+            model="gpt-3.5-turbo",
+            messages=self.conversation,
         )
         result = ''
         for choice in response.choices:
@@ -26,16 +29,31 @@ class ChatGPT():
         self.conversation.append({"role": "system", "content": result})
         return result
 
+    def get_music(self):
+        conversation_music = self.conversation.copy()
+        conversation_music.append(
+            {"role": "user", "content": "Give me a genre of music that fits the current situation in the story. Answer in a single word."})
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation_music,
+        )
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content
+        # remove punctuation
+        result = result.replace('.', '')
+        return result
+
     def get_conversation(self):
         return self.conversation
-    
-    def reset_conversation(self):
-        self.conversation = [
-            {"role": "system", "content": "You are a storyteller. Generate a text-based game adventure with 10 stages and 4 options per stage. Wait for my response at every stage."}
-        ]
-    
-chat = ChatGPT()
-chat.get_response("Start game")
-chat.get_response("Take option 1")
 
-print(chat.get_conversation())
+    def reset_conversation(self):
+        self.conversation = initPrompt
+
+
+chat = ChatGPT()
+chat.get_story("Start game")
+# chat.get_response("Take option 1")
+print(chat.get_music())
+
+# print(chat.get_conversation())

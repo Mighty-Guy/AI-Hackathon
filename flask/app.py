ec916262978812fb20a6da64aa698b311bd7d645
Thomas Huber <thomashuber96@gmx.at>
2023-03-11 15:14:27 +0100

    feat(flask): change start path

--- flask/app.py
+++ flask/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, send_from_directory, render_template
 
 app = Flask(__name__)
 
-@app.route('/record')
+@app.route('/')
 def record():
     return render_template('record.html')
 

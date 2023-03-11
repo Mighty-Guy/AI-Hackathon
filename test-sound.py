4621fcf6be470c618f774e1c76e8d68197d6b763
Matthias Malter <mmalter.its-b2018@fh-salzburg.ac.at>
2023-03-11 15:15:54 +0100

    commit changes

---
+++ test-sound.py
@@ -0,0 +1,14 @@
+from sclib import SoundcloudAPI, Track, Playlist
+
+api = SoundcloudAPI()  # never pass a Soundcloud client ID that did not come from this library
+interpret = 'itsmeneedle'
+song = 'sunday-morning'
+
+track = api.resolve('https://soundcloud.com/' + interpret + '/')
+
+assert type(track) is Track
+
+filename = f'./{track.artist} - {track.title}.mp3'
+
+with open(filename, 'wb+') as file:
+    track.write_mp3_to(file)

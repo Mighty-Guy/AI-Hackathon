e39c0a3b8efb29a723fd93cd3aa182b845ccb179
Thomas Huber <thomashuber96@gmx.at>
2023-03-12 09:54:42 +0100

    feat(flask): update styling

--- flask/app.py
+++ flask/app.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 
 game_options = {0: 'random', 1: 'adventure', 2: 'sci-fy'}
 
+game_text_list = []
+game_text_list.append(game_text)
+game_answers_list = []
+
+game_answers_list.append('test123')
 
 @app.route('/')
 def index():
@@ -19,24 +24,27 @@ def index():
 def play():
     if 'game_option' in request.args:
         option = request.args['game_option']
-
     else:
         option = 0 # default is random
+
 
 
     #add chatgpt
     #play sound
     #styling
-    return render_template('play.html', game_text=game_text)
+    return render_template('play.html', game_text_list=game_text_list, game_answers_list=game_answers_list)
 
 @app.route('/save-audio', methods=['POST'])
 def save_audio():
+    global game_answers_list, game_text_list
     file = request.files['audio']
     file.save('audio.webm')
     whipser = Whisper()
     text = whipser.set_input('audio.webm')
+    game_answers_list.append(text)
     print(text)
     game_text = chat.get_story(text)
+    game_answers_list.append(game_text)
     print(game_text)
     t2s = T2S()
     t2s.get_speech(game_text, './resources/audio-answer.mp3')

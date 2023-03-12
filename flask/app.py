05d825299da85b218909669f3d349a8bf74769b7
Thomas Huber <thomashuber96@gmx.at>
2023-03-12 10:36:03 +0100

    feat(flask): update names

--- flask/app.py
+++ flask/app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 
 game_options = {0: 'random', 1: 'adventure', 2: 'sci-fy'}
 
-game_text_list = []
-game_text_list.append(game_text)
-game_answers_list = []
+game_text_gpt_list = []
+game_text_gpt_list.append(game_text)
+game_user_answers_list = []
 
 
 @app.route('/')
@@ -26,19 +26,19 @@ def play():
     else:
         option = 0 # default is random
 
-    return render_template('play.html', game_text_list=game_text_list, game_answers_list=game_answers_list)
+    return render_template('play.html', game_text_gpt_list=game_text_gpt_list, game_user_answers_list=game_user_answers_list)
 
 @app.route('/save-audio', methods=['POST'])
 def save_audio():
-    global game_answers_list, game_text_list
+    global game_user_answers_list, game_text_gpt_list
     file = request.files['audio']
     file.save('audio.webm')
     whipser = Whisper()
     text = whipser.set_input('audio.webm')
-    game_answers_list.append(text)
+    game_user_answers_list.append(text)
     print(text)
     game_text = chat.get_story(text)
-    game_text_list.append(game_text)
+    game_text_gpt_list.append(game_text)
     print(game_text)
     t2s = T2S()
     t2s.get_speech(game_text, './resources/audio-answer.mp3')
@@ -50,11 +50,11 @@ def serve_audio(text):
 
 @app.route('/chat/gpt/newest')
 def get_newest_gpt_answer():
-    return jsonify({"text": game_text_list[-1]})
+    return jsonify({"text": game_text_gpt_list[-1]})
 
 @app.route('/chat/user/newest')
 def get_newest_user_answer():
-    return jsonify({"text": game_answers_list[-1]})
+    return jsonify({"text": game_user_answers_list[-1]})
 
 if __name__ == '__main__':
     app.run()
